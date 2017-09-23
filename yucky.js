7e47fa6a157f8191bd1af0b4fedd495ae005540a
vincens2005 <bokheadpictures@gmail.com>
2017-09-23 16:49:04 -0700

    Create yucky.js

---
+++ yucky.js
@@ -0,0 +1,12 @@
+$("button").hover(
+  function() {
+    var self = this;
+    var pos = -(Math.floor(Math.random() * 7) + 1) * 55;
+    setTimeout(function() {
+      $(self).find("ul").css("margin-top", pos + "px");
+    }, 500);
+  },
+  function() {
+    $(this).find("ul").css("margin-top", "0px");
+  }
+);
