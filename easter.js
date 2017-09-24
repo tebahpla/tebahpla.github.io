0fb41cc3d4bc46469cc9d3c6a93795919dd22544
vincens2005 <bokheadpictures@gmail.com>
2017-09-24 10:12:56 -0700

    Create easter.js

---
+++ easter.js
@@ -0,0 +1,3 @@
+if(window.location == 'https://tebahpla.github.io/oogle#gsc.tab=0&gsc.q=google&gsc.sort='){
+  window.location='https://www.google.com/search?newwindow=1&source=hp&q=Google&oq=Google';
+  }
