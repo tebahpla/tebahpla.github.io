fa1e6c98381365bb8caee50006db1c160586d6fd
vincens2005 <bokheadpictures@gmail.com>
2017-10-21 09:36:16 -0700

    Update time.js

--- stuff/nomentum/time.js
+++ stuff/nomentum/time.js
@@ -1,7 +1,15 @@
-var d = new Date();
+
+var i = 0;
+
+function timedCount() {
+   var d = new Date();
     var d = new Date(); // for now
 n = d.getHours(); // => 9
 b = d.getMinutes();
+ i= n+":"+b   ;
+postMessage(i);
+    setTimeout("timedCount()",500);
+}
 
-var time= n+":"+b   ;
-postMessage(time);
+timedCount();
+
